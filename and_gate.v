4c34972a246858d2ad4d0d49fca9b5babe94456a
mgsrisarvesanece-web <mgsrisarvesanece@gmail.com>
2026-02-15 19:09:58 +0530

    Add files via upload
    Added RTL and testbench for 2-input AND gate

---
+++ and_gate.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.02.2026 22:21:03
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+    input a,
+    input b,
+    output y
+    );
+    assign y = a&b;
+endmodule
---
+++ and_gate_tb.v
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+
+module and_gate_tb;
+
+reg a;
+reg b;
+wire y;
+
+and_gate uut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a = 0; b = 0;
+    #10 a = 0; b = 1;
+    #10 a = 1; b = 0;
+    #10 a = 1; b = 1;
+    #10 $finish;
+end
+
+endmodule
